61b9417be7f55f7facd33e8077a89ef1822291cd
ravendraarief <ravendraarief@gmail.com>
2024-09-10 17:39:44 +1000

    fix styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-black">
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 p-4">
         <h1 className="font-bold text-[#c2a500]">We'll be back soon!</h1>
         <p className="w-[400px] text-[#c2a500]">
           Our website is currently down for maintenance. We apologize for the
