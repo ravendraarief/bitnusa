f21d8d92c01e9f17cd34cd1ddd23176d9aacaee7
ravendraarief <ravendraarief@gmail.com>
2024-09-10 17:46:20 +1000

    fix styling on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-black">
-      <div className="flex flex-col gap-4 p-4">
-        <h1 className="font-bold text-[#c2a500]">We'll be back soon!</h1>
-        <p className="w-[400px] text-[#c2a500]">
+      <div className="flex flex-col gap-4 p-8">
+        <h1 className="font-bold text-[#c2a500] p-4">We'll be back soon!</h1>
+        <p className="w-[400px] text-[#c2a500] p-4">
           Our website is currently down for maintenance. We apologize for the
           inconvenience and appreciate your patience.
         </p>
-        <p className="text-[#c2a500]">Thank you for understanding.</p>
+        <p className="text-[#c2a500] p-4">Thank you for understanding.</p>
       </div>
     </div>
   );
