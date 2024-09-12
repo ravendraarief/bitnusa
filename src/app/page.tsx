8d6fb2fff097c25367a6ba5c6a58bf9e2f7c6539
ravendraarief <ravendraarief@gmail.com>
2024-09-13 00:04:11 +1000

    design mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import phone from '@/assets/iPhone15Plus.png';
 import iconArrow from '@/assets/icon-arrow.png';
 import workPlace from '@/assets/workplaceimage.png';
 import ecommerceProduct from '@/assets/imageProduct.png';
+import MobileNav from '@/components/MobileNav';
 export default function Home() {
   return (
     <>
-      <Header />
-      <main className="relative min-h-screen bg-slate-100">
+      {/* <Header /> */}
+      <MobileNav />
+      {/* <main className="relative min-h-screen bg-slate-100">
         <section className="pt-8 min-h-screen">
           <div className=" relative flex">
             <div className="absolute top-0 z-10">
@@ -73,6 +75,22 @@ export default function Home() {
             <Image src={ecommerceProduct} width={500} height={300} alt="" />
           </div>
         </section>
+      </main> */}
+
+      <main className="">
+        <div className="relative">
+          <div className="flex justify-center items-center">
+            <Image src={phone} width={250} height={300} alt="" />
+          </div>
+          <div className=" mx-auto w-[250px] h-[150px] bg-white  bg-opacity-60 backdrop-filter backdrop-blur-lg absolute top-10  items-center  rounded-xl p-8  z-40">
+            <div className="flex justify-between items-center">
+              <p className="font-bold uppercase w-[150px]">
+                Transforming Ideas Reality
+              </p>
+              <Image src={iconArrow} alt="" width={40} height={40} />
+            </div>
+          </div>
+        </div>
       </main>
 
       <Footer />
