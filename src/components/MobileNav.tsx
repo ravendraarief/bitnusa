5f4f5f0a8d7cfd2ec92f4e85b50ebbef84bc85f6
ravendraarief <ravendraarief@gmail.com>
2024-09-13 01:29:07 +1000

    update Mobile navigation

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -8,7 +8,7 @@ const MobileNav = () => {
   const [visible, setVisble] = useState(false);
 
   return (
-    <div className="sticky top-0 z-50 ">
+    <div className="sticky top-0 z-50 md:hidden ">
       <div className="p-6 flex justify-between bg-white  bg-opacity-60 backdrop-filter backdrop-blur-lg">
         <div>
           <Logo />
