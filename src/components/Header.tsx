5b74bcc7448229567e535ff286d1ecb48ee9573e
ravendraarief <ravendraarief@gmail.com>
2024-10-24 01:33:41 +1000

    Adjust header and add logo link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Logo from './Logo';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <nav className=" hidden lg:flex justify-between items-center sticky top-0 bg-[#161616] px-5 z-50">
       <Logo />
       <div className="flex space-x-4 items-center">
-        <p className="text-white font-light font-neuemont">Our Services</p>
+        <Link
+          href="#ourservices"
+          className="text-white font-light font-neuemont"
+        >
+          Our Services
+        </Link>
         <button className="bg-white text-black p-2 font-neuemont">
           Contact Us
         </button>
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,16 +1,20 @@
 import Image from 'next/image';
 import React from 'react';
 import logo from '@/assets/Bitnusa_White_logo.png';
+import Link from 'next/link';
+
 const Logo = () => {
   return (
-    <div>
-      <Image
-        src={logo}
-        width={150}
-        height={52.79}
-        alt="bitnusa-logo"
-        className="w-[100px] md:w-auto "
-      />
+    <div className="w-36">
+      <Link href="/">
+        <Image
+          src={logo}
+          width={150}
+          height={52.79}
+          alt="bitnusa-logo"
+          className="w-[100px] md:w-auto "
+        />
+      </Link>
     </div>
   );
 };
