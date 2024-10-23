ca043be90b4bf2009139a5715290b71f4d9e6574
ravendraarief <ravendraarief@gmail.com>
2024-10-23 12:19:17 +1000

    Started for new landing pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,78 +12,12 @@ export default function Home() {
     <>
       <Header />
       <MobileNav />
-      <main className="">
-        <section className="relative lg:min-h-screen">
-          <div className="relative">
-            <div className=" hidden lg:flex  lg:absolute lg:top-0 lg:z-10">
-              <Image src={grid} width={450} height={200} alt="grid" />
-            </div>
-            <div className="z-20 flex flex-col  justify-center  mb-20 mt-10 lg:w-[400px] lg:absolute lg:top-20 lg:left-16">
-              <p className="text-center mb-4 lg:hidden">Bitnusa</p>
-              <p className=" text-xl text-center font-bold lg:text-left lg:text-3xl px-12">
-                Leading Digital Solutions for Your Business
-              </p>
-            </div>
-          </div>
-          <div className="relative lg:min-h-screen">
-            <div className="flex justify-center items-center lg:absolute right-10">
-              <Image src={phone} width={250} height={300} alt="" />
-            </div>
-            <div className="w-[250px] h-[150px] bg-white bg-opacity-60 backdrop-filter backdrop-blur-lg absolute top-10 items-center  rounded-xl p-8 z-40 lg:top-[200px] lg:right-[200px]  ">
-              <div className="flex justify-between items-center">
-                <p className="text-sm font-bold uppercase w-[150px]">
-                  Transforming Ideas Reality
-                </p>
-                <Image src={iconArrow} alt="" width={40} height={40} />
-              </div>
-              <p className="text-xs pt-2">
-                Where creativity meets strategy to transform your brand's vision
-              </p>
-            </div>
-            <div className="lg:hidden w-[250px] h-[150px] bg-white bg-opacity-60 backdrop-filter backdrop-blur-lg absolute top-60 right-10 items-center  rounded-xl p-8 z-40 lg:top-[200px] lg:left-[200px]  ">
-              <div className="flex justify-between items-center">
-                <p className="text-sm font-bold uppercase w-[150px]">
-                  Transforming Ideas Reality
-                </p>
-                <Image src={iconArrow} alt="" width={40} height={40} />
-              </div>
-              <p className="text-xs pt-2">
-                Where creativity meets strategy to transform your brand's vision
-              </p>
-            </div>
-            <div className="relative">
-              <div className="hidden lg:flex lg:absolute lg:top-[250px] lg:left-[50px] lg:z-40">
-                <Image src={workPlace} width={450} height={450} alt="" />
-              </div>
-              <div className="hidden w-[250px] h-[150px] bg-white bg-opacity-60 backdrop-filter backdrop-blur-lg absolute top-60 right-10 items-center  rounded-xl p-8 z-40 lg:block lg:top-[450px] lg:left-[350px]  ">
-                <div className="flex justify-between items-center">
-                  <p className="text-sm font-bold uppercase w-[150px]">
-                    Transforming Ideas Reality
-                  </p>
-                  <Image src={iconArrow} alt="" width={40} height={40} />
-                </div>
-                <p className="text-xs pt-2">
-                  Where creativity meets strategy to transform your brand's
-                  vision
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="relative p-12 lg:p-20">
-          <div className="flex flex-col gap-4 lg:flex-row justify-center items-center">
-            <div className="flex flex-col gap-3 lg:p-14">
-              <p className="text-center lg:text-left">E-commerce Specialist</p>
-              <p className="font-bold text-xl">
-                Comprehensive E-Commerce Development Services
-              </p>
-              <p className="lg:pr-16">
-                We create custom e-commerce platforms that provide a seamless,
-                secure, and scalable shopping experience.
-              </p>
-            </div>
-            <Image src={ecommerceProduct} width={500} height={300} alt="" />
-          </div>
+      <main className="lg:min-h-screen">
+        <section className="px-4 pt-8 lg:px-16 lg:py-8">
+          <h1 className=" lg:text-[80px] xl:text-[100px] 2xl:text-[120px] text-white uppercase tracking-tight leading-none">
+            Leading <br /> Digital Solutions <br />
+            for Your Business
+          </h1>
         </section>
       </main>
 
